ab7613644f8625ff7fab45d3f5c94af029fd047a
Joon Heo <heojoon48@gmail.com>
2022-02-21 14:22:02 +0900

    Update createDBandUser.sql

--- createDBandUser.sql
+++ createDBandUser.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE guacamole_db;
 CREATE USER 'guacamole_user'@'localhost' IDENTIFIED BY 'guacamolePassword';
-CREATE USER 'guacamole_user'@'172.29.%' IDENTIFIED BY 'guacamolePassword';
+CREATE USER 'guacamole_user'@'172.%' IDENTIFIED BY 'guacamolePassword';
 
-GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'172.29.%';
-GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'172.%';
+GRANT ALL ON guacamole_db.* TO 'guacamole_user'@'localhost';
 FLUSH PRIVILEGES;
